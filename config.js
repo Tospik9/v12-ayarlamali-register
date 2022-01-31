eeb1f3020dbdcd6e764206349373f90c91b5c762
Tospik9 <98765625+Tospik9@users.noreply.github.com>
2022-01-31 19:15:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    OwnerID: "",
-    GuildID: "",
+    OwnerID: "829348896891338774",
+    GuildID: "935941090861781012",
     MongoURL: "",
     Prefix: ["!", "."],
 
@@ -9,8 +9,8 @@ module.exports = {
     SecondaryTag: "•",
 
     EmbedFooter: "Developed By Striga",
-    Client_Token: "",
-    Client_VoiceID: "",
+    Client_Token: "OTM3NzQ5MjMzMDU3MDE3OTQ2.YfgRBw.NsT8EMBPktphxX2eGNBmi28kmm4",
+    Client_VoiceID: "937307348370862141",
     Client_Status: "dnd",
     Client_Message: "Striga Code"
 
